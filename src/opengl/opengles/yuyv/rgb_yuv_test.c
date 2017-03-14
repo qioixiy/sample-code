3954fe59f13abc27ff3c736f2d04fc06db152820
qioixiy <qioixiy@gmail.com>
2017-03-14 14:02:35 +0800

    rgb_yuv_test.c add testcase

--- src/opengl/opengles/yuyv/rgb_yuv_test.c
+++ src/opengl/opengles/yuyv/rgb_yuv_test.c
@@ -14,9 +14,9 @@ vec3 yuv_rgb(vec3 yuv)
     G = 1.164*(Y - 16.0) - 0.813*(V - 128.0) - 0.391*(U - 128.0);
     R = 1.164*(Y - 16.0) + 1.596*(V - 128.0);
 
-    R = R < 0.0 ? 0.0 : R;
-    G = G < 0.0 ? 0.0 : G;
-    B = B < 0.0 ? 0.0 : B;
+    /* R = R < 0.0 ? 0.0 : R; */
+    /* G = G < 0.0 ? 0.0 : G; */
+    /* B = B < 0.0 ? 0.0 : B; */
 
     vec3 rgb;
     rgb.r = R/256.0;
@@ -44,6 +44,66 @@ vec3 rgb_yuv(vec3 rgb)
     yuv.b = Cr/256.0;
 
     return yuv;
+}
+
+void test_yuv_rgb()
+{
+    int y = 0, u = 0, v = 0;
+    for (y = 0; y < 256; y++) {
+        for (u = 0; u < 256; u++) {
+            for (v = 0; v < 256; v++) {
+                vec3 yuv = {y/256.0, u/256.0, v/256.0};
+                vec3 rgb = yuv_rgb(yuv);
+                printf("yuv_rgb %d %d %d %d %d %d\n", y, u, v, (int)(rgb.r*256), (int)(rgb.g*256), (int)(rgb.b*256));
+            }
+        }
+    }
+}
+
+void test_rgb_yuv()
+{
+    int r = 0, g = 0, b = 0;
+    for (r = 0; r < 256; r++) {
+        for (g = 0; g < 256; g++) {
+            for (b = 0; b < 256; b++) {
+                vec3 rgb = {r/256.0, g/256.0, b/256.0};
+                vec3 yuv = rgb_yuv(rgb);
+                printf("rgb_yuv %d %d %d %d %d %d\n", r, g, b, (int)(yuv.r*256), (int)(yuv.g*256), (int)(yuv.b*256));
+            }
+        }
+    }
+}
+
+void test_rgb_yuv_rgb()
+{
+    int r = 0, g = 0, b = 0;
+    for (r = 0; r < 256; r++) {
+        for (g = 0; g < 256; g++) {
+            for (b = 0; b < 256; b++) {
+                vec3 rgb = {r/256.0, g/256.0, b/256.0};
+                vec3 yuv = rgb_yuv(rgb);
+                printf("<rgb_yuv %d %d %d %d %d %d\n", r, g, b, (int)(yuv.r*256), (int)(yuv.g*256), (int)(yuv.b*256));
+                rgb = yuv_rgb(yuv);
+                printf(">yuv_rgb %d %d %d %d %d %d\n", (int)(yuv.r*256), (int)(yuv.g*256), (int)(yuv.b*256), (int)(rgb.r*256), (int)(rgb.g*256), (int)(rgb.b*256));
+            }
+        }
+    }
+}
+
+void test_yuv_rgb_yuv()
+{
+    int r = 0, g = 0, b = 0;
+    for (r = 0; r < 256; r++) {
+        for (g = 0; g < 256; g++) {
+            for (b = 0; b < 256; b++) {
+                vec3 yuv = {r/256.0, g/256.0, b/256.0};
+                vec3 rgb = yuv_rgb(yuv);
+                printf("<yuv_rgb %d %d %d %d %d %d\n", (int)(yuv.r*256), (int)(yuv.g*256), (int)(yuv.b*256), (int)(rgb.r*256), (int)(rgb.g*256), (int)(rgb.b*256));
+                yuv = rgb_yuv(rgb);
+                printf(">rgb_yuv %d %d %d %d %d %d\n", (int)(rgb.r*256), (int)(rgb.g*256), (int)(rgb.b*256), (int)(yuv.r*256), (int)(yuv.g*256), (int)(yuv.b*256));
+            }
+        }
+    }
 }
 
 int main()
@@ -107,6 +167,22 @@ int main()
     printf (" rgb_yuv\n");
     yuv = rgb_yuv(rgb);
     printf("yuv: %f %f %f\n", yuv.r, yuv.g, yuv.b);
+
+    rgb.r = 0xff/255.0f;
+    rgb.g = 0xff/255.0f;
+    rgb.b = 0xff/255.0f;
+    printf("*** test5 0xff 0xff 0xff\n rgb_yuv\n");
+    printf("rgb: %f %f %f\n", rgb.r, rgb.g, rgb.b);
+    printf (" rgb_yuv\n");
+    yuv = rgb_yuv(rgb);
+    printf("yuv: %f %f %f\n", yuv.r, yuv.g, yuv.b);
+    rgb = yuv_rgb(yuv);
+    printf("rgb: %f %f %f\n", rgb.r, rgb.g, rgb.b);
+
+    /* test_rgb_yuv(); */
+    /* test_yuv_rgb(); */
+    /* test_rgb_yuv_rgb(); */
+    test_yuv_rgb_yuv();
 
     printf("done\n");
 }
