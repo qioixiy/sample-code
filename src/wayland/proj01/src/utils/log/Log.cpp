6a3601041b139bd170998b6b50fff09758d08db5
qioixiy <qioixiy@gmail.com>
2017-08-05 17:57:56 +0800

    src/utils/log/Log update

--- src/wayland/proj01/src/utils/log/Log.cpp
+++ src/wayland/proj01/src/utils/log/Log.cpp
@@ -1,5 +1,11 @@
+#include <string.h>
 #include <ctime>
+#include <stdlib.h>
+#include <unistd.h>
+#include <linux/unistd.h>
+#include <sys/syscall.h>   /* For SYS_xxx definitions */
 #include <sys/time.h>
+#include <sys/types.h>
 #include <fstream>
 #include <iostream>
 #include <algorithm>
@@ -7,18 +13,56 @@
 #include <iomanip>
 #include "Log.hpp"
 
-#define ANSI_RED(fmt) ("\x1b[31m" fmt "\x1b[0m")
-#define ANSI_GREEN(fmt) ("\x1b[32m" fmt "\x1b[0m")
-#define ANSI_BROWN(fmt) ("\x1b[33m" fmt "\x1b[0m")
-#define ANSI_BLUE(fmt) ("\x1b[33m" fmt "\x1b[0m")
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
 
-#define _ANSI_RED "\x1b[31m"
-#define _ANSI_HIGHBRIGHTNESS_RED "\x1b[1;31m"
-#define _ANSI_GREEN "\x1b[32m"
-#define _ANSI_BROWN "\x1b[33m"
-#define _ANSI_BLUE "\x1b[34m"
+#ifndef gettid
+#define gettid() syscall(__NR_gettid)
+#endif
 
-#define ANSI_RESET "\x1b[0m"
+// "\x1b[0m" or "\e[0m"
+#define NONE                 "\e[0m"
+#define BLACK                "\e[0;30m"
+#define L_BLACK              "\e[1;30m"
+#define RED                  "\e[0;31m"
+#define L_RED                "\e[1;31m"
+#define GREEN                "\e[0;32m"
+#define L_GREEN              "\e[1;32m"
+#define BROWN                "\e[0;33m"
+#define YELLOW               "\e[1;33m"
+#define BLUE                 "\e[0;34m"
+#define L_BLUE               "\e[1;34m"
+#define PURPLE               "\e[0;35m"
+#define L_PURPLE             "\e[1;35m"
+#define CYAN                 "\e[0;36m"
+#define L_CYAN               "\e[1;36m"
+#define GRAY                 "\e[0;37m"
+#define WHITE                "\e[1;37m"
+
+#define BOLD                 "\e[1m"
+#define UNDERLINE            "\e[4m"
+#define BLINK                "\e[5m"
+#define REVERSE              "\e[7m"
+#define HIDE                 "\e[8m"
+#define CLEAR                "\e[2J"
+#define CLRLINE              "\r\e[K" //or "\e[1K\r"
+
+#define ANSI_NONE NONE
+
+#define ANSI_RED(fmt) (RED##fmt##NONE)
+#define ANSI_GREEN(fmt) (GREEN##fmtNONE)
+#define ANSI_BROWN(fmt) (BROWN##fmtNONE)
+#define ANSI_BLUE(fmt) (BLUE##fmt##NONE)
+#define ANSI_YELLOW(fmt) (YELLOW##fmt##NONE)
+
+#define _ANSI_RED RED
+#define _ANSI_GREEN GREEN
+#define _ANSI_BROWN BROWN
+#define _ANSI_BLUE BLUE
+#define _ANSI_GRAY GRAY
+#define _ANSI_L_RED L_RED
+#define _ANSI_YELLOW YELLOW
 
 #ifdef USE_LOG_CAT
 #include <base/log/Log.h>
@@ -28,7 +72,78 @@
 bool LoggerStream::useLogCat = true;
 #endif
 std::string LoggerStream::TAG = "";
+bool LoggerStream::useFileBackEnd = false;
+std::ofstream LoggerStream::logOFStream;
+int LoggerStream::fileMaxSize = 40 * 1024 *1024; // default: 40M
+const char* LoggerStream::logPath = "/data/srv.log";
 
+class LogLevel {
+public:
+    LogLevel() {
+        char* str = getenv("LOG_LEVEL");
+        if (str != NULL) {
+            static struct {
+                LoggerType type;
+                const std::string str;
+            } arr[] = {
+                {E_Debug, "D"},
+                {E_Info, "I"},
+                {E_Warning, "W"},
+                {E_Critical, "C"},
+                {E_Error, "E"},
+                {E_FastBoot, "F"},
+            };
+
+            for (auto &item : arr) {
+                if (item.str == str) {
+                    limit = item.type;
+                    break;
+                }
+            }
+        }
+    }
+
+    enum LoggerType getLimit() {
+        return limit;
+    }
+
+    LoggerType limit = E_Debug;
+};
+
+class LogFileFeature {
+public:
+    LogFileFeature() {
+        char* str = getenv("LOG_TOFILE");
+        if (str != NULL) {
+            if (std::string("1") == str) {
+                LoggerStream::useFileBackEnd = true;
+            }
+        }
+        str = getenv("LOG_FILE_PATH");
+        if (str != NULL) {
+            LoggerStream::logPath = strdup(str);
+        }
+    }
+};
+
+class LogFileMaxSize {
+public:
+    LogFileMaxSize() {
+        char* str = getenv("LOG_FILE_MAXSIZE");
+        if (str != NULL) {
+            int size = atoi(str);
+            if (size != 0) {
+                LoggerStream::fileMaxSize = size;
+            }
+        }
+    }
+};
+
+static LogLevel logLevel;
+static LogFileFeature logFileFeature;
+static LogFileMaxSize logFileMaxSize;
+
+// class LoggerStream
 LoggerStream::LoggerStream()
     : stream(new Stream()) {}
 
@@ -37,31 +152,44 @@ LoggerStream::LoggerStream(std::string *s)
 
 LoggerStream::~LoggerStream()
 {
-    std::ostringstream sstimestamp;
-    mkTimeSpec0(sstimestamp);
+    // depend on env["LOG_LEVEL"]
+    if (logLevel.getLimit() <= stream->loggerType) {
+        std::ostringstream sstimestamp;
+        mkTimeSpec0(sstimestamp);
 
-    toConsole(stream->loggerType, stream->context,
-              stream->ss.str(), sstimestamp);
+        toConsole(stream->loggerType, stream->context,
+                  stream->ss.str(), sstimestamp);
+        if (useFileBackEnd) {
+            toFile(stream->loggerType, stream->context,
+                   stream->ss.str(), sstimestamp);
+        }
 #ifdef USE_LOG_CAT
-    if (useLogCat)
-    {
-        toLogCat(stream->loggerType, stream->context,
-                 stream->ss.str(), sstimestamp);
-    }
+        if (useLogCat) {
+            toLogCat(stream->loggerType, stream->context,
+                     stream->ss.str(), sstimestamp);
+        }
 #endif
+    }
+
     delete stream;
 }
 
 void
-LoggerStream::mkTag(std::ostringstream &ss)
+LoggerStream::SetTag(std::string tag)
 {
-    ss << TAG;
+    TAG = tag;
 }
 
 void
-LoggerStream::mkTimeSpec0(std::ostringstream &ss)
+LoggerStream::mkTag(std::ostringstream& ss)
 {
-    char buf[40] = "";
+    ss << TAG.c_str();
+}
+
+void
+LoggerStream::mkTimeSpec0(std::ostringstream& ss)
+{
+    char buf[40]="";
 #define USEC_TO_SEC(usec) (usec/1000000.0)
     struct timeval current;
     double time;
@@ -71,55 +199,41 @@ LoggerStream::mkTimeSpec0(std::ostringstream &ss)
     ss << buf;
 }
 void
-LoggerStream::mkTimeSpec1(std::ostringstream &ss)
+LoggerStream::mkTimeSpec1(std::ostringstream& ss)
 {
 #define MST (-7)
 #define UTC (0)
 #define CCT (+8)
     struct timeval tv;
     gettimeofday(&tv, NULL);
-    struct tm *ptm = gmtime(&tv.tv_sec);
+    struct tm * ptm = gmtime(&tv.tv_sec);
 
     ss << '[';
-    ss << 1900 + ptm->tm_year << '-'
-       << std::setw(2) << std::setfill('0') << 1 + ptm->tm_mon << '-'
+    ss << 1900+ptm->tm_year << '-'
+       << std::setw(2) << std::setfill('0') << 1+ptm->tm_mon << '-'
        << std::setw(2) << std::setfill('0') << ptm->tm_mday << ' ' ;
-    ss << std::setw(2) << std::setfill('0') << (ptm->tm_hour + CCT) % 24 << ':'
+    ss << std::setw(2) << std::setfill('0') << (ptm->tm_hour+CCT)%24 << ':'
        << std::setw(2) << std::setfill('0') << ptm->tm_min << ':'
        << std::setw(2) << std::setfill('0') << ptm->tm_sec << ':'
        << std::setw(6) << std::setfill('0') << tv.tv_usec << std::setw(0);
     ss << ']';
 }
 
-const char *
+const char*
 LoggerStream::mkType(LoggerType type,
                      const LoggerContext &context,
-                     std::ostringstream &ss)
+                     std::ostringstream& ss)
 {
-    const char *ret = "";
+    const char* ret = "";
     ss << "[";
-    switch (type)
-    {
+    switch (type) {
     default:
-    case E_Debug:
-        ss << "D";
-        break;
-    case E_Info:
-        ss << "I";
-        ret = _ANSI_BLUE;
-        break;
-    case E_Warning:
-        ss << "W";
-        ret = _ANSI_BROWN;
-        break;
-    case E_Critical:
-        ss << "C";
-        ret = _ANSI_HIGHBRIGHTNESS_RED;
-        break;
-    case E_Error:
-        ss << "E";
-        ret = _ANSI_RED;
-        break;
+    case E_Debug: ss << "D"; ret = _ANSI_GRAY; break;
+    case E_Info: ss << "I"; ret = _ANSI_BLUE; break;
+    case E_Warning: ss << "W"; ret = _ANSI_BROWN; break;
+    case E_Critical:ss << "C"; ret = _ANSI_L_RED; break;
+    case E_Error: ss << "E"; ret = _ANSI_RED; break;
+    case E_FastBoot: ss << "F"; ret = _ANSI_YELLOW; break;
     }
     ss << "]";
 
@@ -129,35 +243,50 @@ LoggerStream::mkType(LoggerType type,
 class map_finder
 {
 public:
-    map_finder(const char *_s): str(_s) {}
-    bool operator ()(const std::map<const char *, int>::value_type &pair)
-    {
+    map_finder(const char* _s):str(_s){}
+    bool operator ()(const std::map<const char*, int>::value_type &pair){
         return pair.first == str;
     }
 private:
-    const char *str;
+    const char* str;
 };
-static const char *skipFilePathDir(const char *s)
+static const char* skipFilePathDir(const char* s)
 {
-    std::map<const char *, int> mapFiles;
-    const std::map<const char *, int>::iterator iter
+    std::map<const char*, int> mapFiles;
+    const std::map<const char*, int>::iterator iter
         = std::find_if(mapFiles.begin(), mapFiles.end(), map_finder(s));
-    if (iter == mapFiles.end())
-    {
-        mapFiles[s] = 1 + std::string(s).rfind('/');
-        return s + mapFiles[s];
+    if (iter == mapFiles.end()) {
+        mapFiles[s] = 1+std::string(s).rfind('/');
+        return s+mapFiles[s];
     }
     return &s[iter->second];
 }
+
 void
 LoggerStream::mkBannaer(LoggerType type,
                         const LoggerContext &context,
-                        std::ostringstream &ss)
+                        std::ostringstream& ss)
 {
     // " funtion,file+Lno: "
     ss << " " << context.function << ","
        << skipFilePathDir(context.file)
-       << "+L" << context.line << ": ";
+       << ",L" << context.line << ": ";
+}
+
+void
+LoggerStream::mkOuput(LoggerType type, const LoggerContext &context,
+                      const std::string &logBuffer,
+                      const std::ostringstream &sstimestamp,
+                      std::ostringstream& ss)
+{
+    ss << _ANSI_GREEN << sstimestamp.str() << ANSI_NONE;
+    ss << _ANSI_BROWN << ' ';
+    mkTag(ss);
+    ss << ANSI_NONE;
+    const char* color = mkType(type, context, ss);
+    mkBannaer(type, context, ss);
+
+    ss << color << logBuffer << ANSI_NONE;
 }
 
 void
@@ -167,29 +296,38 @@ LoggerStream::toConsole(LoggerType type,
                         const std::ostringstream &sstimestamp)
 {
     std::ostringstream ss;
+    mkOuput(type, context, logBuffer, sstimestamp, ss);
 
-    ss << _ANSI_GREEN << sstimestamp.str() << ANSI_RESET;
-    ss << _ANSI_BROWN;
-    mkTag(ss);
-    ss << ANSI_RESET;
-    const char *color = mkType(type, context, ss);
-    mkBannaer(type, context, ss);
-
-    ss << color << logBuffer << ANSI_RESET;
-
-    switch (type)
-    {
+    switch (type) {
     default:
     case E_Debug:
     case E_Info:
-        std::cout << ss.str() << std::endl;
-        break;
+        std::cout << ss.str() << std::endl; break;
     case E_Warning:
     case E_Critical:
     case E_Error:
-        std::cerr << ss.str() << std::endl;
-        break;
+        std::cerr << ss.str() << std::endl; break;
     }
+}
+
+void
+LoggerStream::toFile(LoggerType type, const LoggerContext &context,
+                     const std::string &logBuffer,
+                     const std::ostringstream &sstimestamp)
+{
+    if (!logOFStream.is_open()) {
+        logOFStream.open(logPath);
+    }
+
+    // relocation
+    if (LoggerStream::logOFStream.tellp() > fileMaxSize) {
+        LoggerStream::logOFStream.seekp(0);
+    }
+
+    std::ostringstream ss;
+    mkOuput(type, context, logBuffer, sstimestamp, ss);
+
+    logOFStream << ss.str() << std::endl;
 }
 
 #ifdef USE_LOG_CAT
@@ -201,7 +339,7 @@ LoggerStream::toLogCat(LoggerType type,
 {
 
 #ifndef LOG_TAG
-#define LOG_TAG "SRV"
+#define LOG_TAG (TAG.c_str())
 #endif
 
     std::ostringstream ss;
@@ -210,24 +348,14 @@ LoggerStream::toLogCat(LoggerType type,
     mkBannaer(type, context, ss);
     ss << logBuffer;
 
-    switch (type)
-    {
+    switch (type) {
     default:
-    case E_Debug:
-        LOG_D("%s", ss.str().c_str());
-        break;
-    case E_Info:
-        LOG_I("%s", ss.str().c_str());
-        break;
-    case E_Warning:
-        LOG_W("%s", ss.str().c_str());
-        break;
-    case E_Critical:
-        LOG_E("%s", ss.str().c_str());
-        break;
-    case E_Error:
-        LOG_E("%s", ss.str().c_str());
-        break;
+    case E_Debug: LOG_D("%s", ss.str().c_str()); break;
+    case E_Info: LOG_I("%s", ss.str().c_str()); break;
+    case E_Warning: LOG_W("%s", ss.str().c_str()); break;
+    case E_Critical: LOG_E("%s", ss.str().c_str()); break;
+    case E_Error: LOG_E("%s", ss.str().c_str()); break;
+    case E_FastBoot: break;
     }
 }
 #endif
@@ -257,6 +385,11 @@ LoggerStream Logger::__xxError() const
     return genLoggerStream(E_Error);
 }
 
+LoggerStream Logger::__xxFastBoot() const
+{
+    return genLoggerStream(E_FastBoot);
+}
+
 LoggerStream Logger::genLoggerStream(enum LoggerType type) const
 {
     LoggerStream ls;
@@ -264,4 +397,19 @@ LoggerStream Logger::genLoggerStream(enum LoggerType type) const
     ctxt.copy(context);
     ls.stream->loggerType = type;
     return ls;
+}
+
+__xxScopeLogger::__xxScopeLogger(std::string tag,
+                                 const char* file,
+                                 const char* func,
+                                 int line)
+    : tag(tag)
+    , file(file)
+    , func(func)
+    , line(line) {
+    Logger(file, func, line).__xxDebug() << tag << "start";
+}
+
+__xxScopeLogger::~__xxScopeLogger() {
+    Logger(file, func, line).__xxDebug() << tag << "done";
 }
--- src/wayland/proj01/src/utils/log/Log.hpp
+++ src/wayland/proj01/src/utils/log/Log.hpp
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <sstream>
+#include <fstream>
 
 //#define NOTUSED() __attribute__((unused))
 #define NOTUSED(v) for (;;) { (void)(1 ? (void)0 : ( (void)(v) ) ); break; }
@@ -13,6 +14,7 @@ enum LoggerType
     E_Warning,
     E_Critical,
     E_Error,
+    E_FastBoot,
     E_Default = E_Debug,
 };
 
@@ -22,13 +24,12 @@ class LoggerContext
     friend class LoggerStream;
 
 public:
-    LoggerContext(const char *_file = "",
-                  const char *_function = "",
-                  int _line = -1)
-        : line(_line), file(_file), function(_function) {}
+LoggerContext(const char *_file="",
+              const char *_function="",
+              int _line = -1)
+    : line(_line), file(_file), function(_function) {}
 
-    void copy(const LoggerContext &cxt)
-    {
+    void copy(const LoggerContext &cxt) {
         file = cxt.file;
         line = cxt.line;
         function = cxt.function;
@@ -43,11 +44,10 @@ private:
 class LoggerStream
 {
 public:
-    struct Stream
-    {
-        Stream(): ss(), space(true), context() {}
-        Stream(std::string *s)
-            : ss(*s, std::ostringstream::ate), space(true), context() {}
+    struct Stream {
+    Stream():ss(), space(true), context() {}
+    Stream(std::string *s)
+    :ss(*s, std::ostringstream::ate), space(true), context(){}
         std::ostringstream ss;
         bool space;
         LoggerType loggerType;
@@ -57,131 +57,76 @@ public:
 public:
     LoggerStream();
     LoggerStream(std::string *s);
-    ~LoggerStream();
+    virtual ~LoggerStream();
+    void SetTag(std::string tag);
 
-    inline LoggerStream &operator<<(bool t)
-    {
-        stream->ss << (t ? "true" : "false");
-        return maybeSpace();
-    }
-    inline LoggerStream &operator<<(char t)
-    {
-        stream->ss << t;
-        return maybeSpace();
-    }
-    inline LoggerStream &operator<<(unsigned char t)
-    {
-        stream->ss << t;
-        return maybeSpace();
-    }
-    inline LoggerStream &operator<<(short t)
-    {
-        stream->ss << t;
-        return maybeSpace();
-    }
-    inline LoggerStream &operator<<(unsigned short t)
-    {
-        stream->ss << t;
-        return maybeSpace();
-    }
-    inline LoggerStream &operator<<(int t)
-    {
-        stream->ss << t;
-        return maybeSpace();
-    }
-    inline LoggerStream &operator<<(unsigned int t)
-    {
-        stream->ss << t;
-        return maybeSpace();
-    }
-    inline LoggerStream &operator<<(long t)
-    {
-        stream->ss << t;
-        return maybeSpace();
-    }
-    inline LoggerStream &operator<<(long long t)
-    {
-        stream->ss << t;
-        return maybeSpace();
-    }
-    inline LoggerStream &operator<<(float t)
-    {
-        stream->ss << t;
-        return maybeSpace();
-    }
-    inline LoggerStream &operator<<(double t)
-    {
-        stream->ss << t;
-        return maybeSpace();
-    }
-    inline LoggerStream &operator<<(std::string s)
-    {
-        stream->ss << s;
-        return maybeSpace();
-    }
-    inline LoggerStream &operator<<(const char *s)
-    {
-        stream->ss << s;
-        return maybeSpace();
-    }
-    inline LoggerStream &space()
-    {
-        stream->space = true;
-        stream->ss << ' ';
-        return *this;
-    }
-    inline LoggerStream &nospace()
-    {
-        stream->space = false;
-        return *this;
-    }
-    inline LoggerStream &maybeSpace()
-    {
-        if (stream->space)
-        {
-            stream->ss << ' ';
-        }
-        return *this;
-    }
+    inline LoggerStream& operator<<(bool t) { stream->ss<<(t ? "true" : "false"); return maybeSpace(); }
+    inline LoggerStream& operator<<(char t) { stream->ss<< t; return maybeSpace(); }
+    inline LoggerStream& operator<<(unsigned char t) { stream->ss<< t; return maybeSpace(); }
+    inline LoggerStream& operator<<(short t) { stream->ss << t; return maybeSpace(); }
+    inline LoggerStream& operator<<(unsigned short t) { stream->ss << t; return maybeSpace(); }
+    inline LoggerStream& operator<<(int t) { stream->ss<< t; return maybeSpace(); }
+    inline LoggerStream& operator<<(unsigned int t) { stream->ss<< t; return maybeSpace(); }
+    inline LoggerStream& operator<<(long t) { stream->ss<< t; return maybeSpace(); }
+    inline LoggerStream& operator<<(long long t) { stream->ss<< t; return maybeSpace(); }
+    inline LoggerStream& operator<<(float t) { stream->ss<< t; return maybeSpace(); }
+    inline LoggerStream& operator<<(double t) { stream->ss<< t; return maybeSpace(); }
+    inline LoggerStream& operator<<(std::string s) { stream->ss << s; return maybeSpace(); }
+    inline LoggerStream& operator<<(const char* s) { stream->ss << s; return maybeSpace(); }
+    inline LoggerStream& operator<<(const void* p) { stream->ss << p; return maybeSpace(); }
+    inline LoggerStream& space() { stream->space = true; stream->ss << ' '; return *this; }
+    inline LoggerStream& nospace() { stream->space = false; return *this; }
+    inline LoggerStream& maybeSpace() { if (stream->space) stream->ss << ' '; return *this; }
 
+private:
     void toConsole(LoggerType type, const LoggerContext &context,
                    const std::string &logBuffer,
                    const std::ostringstream &sstimestamp);
+    void toFile(LoggerType type, const LoggerContext &context,
+                const std::string &logBuffer,
+                const std::ostringstream &sstimestamp);
 #ifdef USE_LOG_CAT
     void toLogCat(LoggerType type, const LoggerContext &context,
                   const std::string &logBuffer,
                   const std::ostringstream &sstimestamp);
 #endif
-
-private:
-    void mkTag(std::ostringstream &ss);
-    void mkTimeSpec0(std::ostringstream &ss);
-    void mkTimeSpec1(std::ostringstream &ss);
-    void mkBannaer(LoggerType, const LoggerContext &, std::ostringstream &ss);
-    const char *mkType(LoggerType, const LoggerContext &, std::ostringstream &ss);
-
+    void mkTag(std::ostringstream& ss);
+    void mkTimeSpec0(std::ostringstream& ss);
+    void mkTimeSpec1(std::ostringstream& ss);
+    void mkBannaer(LoggerType, const LoggerContext &, std::ostringstream& ss);
+    const char* mkType(LoggerType, const LoggerContext &, std::ostringstream& ss);
+    void mkOuput(LoggerType type, const LoggerContext &context,
+                 const std::string &logBuffer,
+                 const std::ostringstream &sstimestamp, std::ostringstream&);
 public:
     struct Stream *stream;
 #ifdef USE_LOG_CAT
     static bool useLogCat;
 #endif
+    /* file backend */
+    static bool useFileBackEnd;
+    static const char* logPath;
+    static int fileMaxSize;
+    static std::ofstream logOFStream;
+    /* global tag */
     static std::string TAG;
 };
 
 class Logger
 {
 public:
-    Logger() : context() {}
-    Logger(const char *fileName, const char *functionName, int lineNumber)
-        : context(fileName, functionName, lineNumber) {}
-    Logger(const char *functionName, int lineNumber)
-        : context("", functionName, lineNumber) {}
+Logger() : context(){}
+Logger(const char *fileName, const char *functionName, int lineNumber)
+    : context(fileName, functionName, lineNumber) {}
+Logger(const char *functionName, int lineNumber)
+    : context("", functionName, lineNumber) {}
 
     LoggerStream __xxDebug() const;
     LoggerStream __xxInfo() const;
     LoggerStream __xxWarning() const;
     LoggerStream __xxCritical() const;
     LoggerStream __xxError() const;
+    LoggerStream __xxFastBoot() const;
 
 private:
     LoggerStream genLoggerStream(enum LoggerType type) const;
@@ -190,14 +135,36 @@ protected:
     LoggerContext context;
 };
 
+class __xxScopeLogger {
+public:
+    __xxScopeLogger(std::string tag, const char*, const char*, int);
+    virtual ~__xxScopeLogger();
+
+private:
+    std::string tag;
+    const char* file;
+    const char* func;
+    int line;
+};
+
+void SetLogLevel(std::string level);
+
 #define LogD Logger(__FILE__, __FUNCTION__, __LINE__).__xxDebug()
 #define LogI Logger(__FILE__, __FUNCTION__, __LINE__).__xxInfo()
 #define LogW Logger(__FILE__, __FUNCTION__, __LINE__).__xxWarning()
 #define LogC Logger(__FILE__, __FUNCTION__, __LINE__).__xxCritical()
 #define LogE Logger(__FILE__, __FUNCTION__, __LINE__).__xxError()
+#define LogF Logger(__FILE__, __FUNCTION__, __LINE__).__xxFastBoot()
 
-#define __LogFormat(...)                              \
-  char __xxs[100];snprintf(__xxs,100,__VA_ARGS__);
+#define DECLARE_VAR__(x, y) x##y
+#define DECLARE_VAR_(x, y) DECLARE_VAR__(x, y)
+/* __LINE__, __COUNTER__ */
+#define DECLARE_VAR(unique_prefix) DECLARE_VAR_(unique_prefix, __LINE__)
+#define ScopeLogger(tag) \
+    __xxScopeLogger DECLARE_VAR(scope_) (tag, __FILE__, __FUNCTION__, __LINE__)
+
+#define __LogFormat(...)                                        \
+    char __xxs[200];snprintf(__xxs,sizeof(__xxs),__VA_ARGS__);
 #ifdef Err
 #undef Err
 #endif
@@ -214,6 +181,8 @@ protected:
 #undef Debug
 #endif
 #define Debug(...) {__LogFormat(__VA_ARGS__); LogD<<__xxs;}
+
+#define FastBoot(...) {__LogFormat(__VA_ARGS__); LogF<<__xxs;}
 
 #ifdef loge
 #undef loge
